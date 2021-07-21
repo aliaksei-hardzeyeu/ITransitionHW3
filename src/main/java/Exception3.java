eaf86f34c31e19ef5c68977fa9ab62d7534b44bc
Alex <fugent89@yandex.ru>
2021-07-21 12:15:44 +0300

    corrected cyrillic symbol in exception3

--- src/main/java/Exception3.java
+++ src/main/java/Exception3.java
@@ -1,5 +1,5 @@
 public class Exception3 extends Exception{
     public Exception3(int i, int j) {
-        System.out.println("You`ve entered equal strings " + (i+1) + " и " + (j+1) + " , all strings should be unique");
+        System.out.println("You`ve entered equal strings " + (i+1) + " and " + (j+1) + " , all strings should be unique");
     }
 }
